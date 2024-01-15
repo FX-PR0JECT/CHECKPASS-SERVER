5b4ba9180b6ee0fc472d52a8d74b0acad95fd128
JIUNG YANG <yangjiung286@gmail.com>
2024-01-15 14:09:06 +0900

    CHECKPASS-158 Refactor : [ Server ] Lecture Class 속성 변경

---
+++ backend/src/main/java/FXPROJECT/CHECKPASS/domain/enums/LectureKind.java
@@ -0,0 +1,18 @@
+package FXPROJECT.CHECKPASS.domain.enums;
+
+public enum LectureKind {
+
+    // 전필, 전선, 자선, 산학, 교양, 교직, 마선, 마필, 융선, 융필, K융필
+
+    MANDATORY("전필"),CHOICE("전선"),AUTONOMY("자선"),INDUSTRY("산학"),CULTURE("교양");
+
+    private String lectureKind;
+
+    LectureKind(String lectureKind) {
+        this.lectureKind = lectureKind;
+    }
+
+    public String getKind() {
+        return lectureKind;
+    }
+}
