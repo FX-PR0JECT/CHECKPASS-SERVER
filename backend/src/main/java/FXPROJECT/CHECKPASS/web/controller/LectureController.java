398ad30652fbdcfaa93a0d56c61962a022ed1e40
JIUNG YANG <yangjiung286@gmail.com>
2024-01-15 14:16:38 +0900

    CHECKPASS-154 Feat : [ Server ] 강의 등록 구현

---
+++ backend/src/main/java/FXPROJECT/CHECKPASS/web/controller/LectureController.java
@@ -0,0 +1,41 @@
+package FXPROJECT.CHECKPASS.web.controller;
+
+import FXPROJECT.CHECKPASS.domain.common.constant.CommonMessage;
+import FXPROJECT.CHECKPASS.domain.common.constant.ErrorCode;
+import FXPROJECT.CHECKPASS.domain.common.constant.State;
+import FXPROJECT.CHECKPASS.domain.entity.lectures.Lecture;
+import FXPROJECT.CHECKPASS.web.common.utils.ResultSetUtils;
+import FXPROJECT.CHECKPASS.web.form.requestForm.letures.register.LectureRegisterForm;
+import FXPROJECT.CHECKPASS.web.form.responseForm.resultForm.ResultForm;
+import FXPROJECT.CHECKPASS.web.service.lectures.LectureService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import static FXPROJECT.CHECKPASS.domain.common.constant.CommonMessage.*;
+import static FXPROJECT.CHECKPASS.domain.common.constant.State.*;
+import static FXPROJECT.CHECKPASS.domain.common.constant.ErrorCode.*;
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/lectures")
+public class LectureController {
+
+    private final LectureService lectureService;
+
+    @PostMapping("/registerLecture")
+    public ResultForm registerLecture(@RequestBody @Validated LectureRegisterForm form, BindingResult bindingResult){
+
+        Lecture lecture = lectureService.transferToLecture(form);
+
+        if(!lectureService.registerLecture(lecture)){
+            return ResultSetUtils.getResultForm(FAIL, EXISTING_LECTURE.getCode(), EXISTING_LECTURE.getTitle(), FAIL_REGISTER.getDescription(),null);
+        }
+
+        return ResultSetUtils.getResultForm(SUCCESS, OK.getCode(), OK.getTitle(), COMPLETE_REGISTER.getDescription(), null);
+
+    }
+}
---
+++ backend/src/main/java/FXPROJECT/CHECKPASS/web/service/lectures/LectureService.java
@@ -0,0 +1,76 @@
+package FXPROJECT.CHECKPASS.web.service.lectures;
+
+
+import FXPROJECT.CHECKPASS.domain.entity.lectures.Lecture;
+import FXPROJECT.CHECKPASS.domain.entity.users.Professor;
+import FXPROJECT.CHECKPASS.domain.repository.lectures.JpaLectureRepository;
+import FXPROJECT.CHECKPASS.web.common.utils.ResultSetUtils;
+import FXPROJECT.CHECKPASS.web.form.requestForm.letures.register.LectureRegisterForm;
+import FXPROJECT.CHECKPASS.web.form.responseForm.resultForm.ResultForm;
+import FXPROJECT.CHECKPASS.web.service.users.UserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import static FXPROJECT.CHECKPASS.domain.common.constant.CommonMessage.*;
+import static FXPROJECT.CHECKPASS.domain.common.constant.State.*;
+import static FXPROJECT.CHECKPASS.domain.common.constant.ErrorCode.*;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class LectureService {
+
+    private final JpaLectureRepository jpaLectureRepository;
+    private final UserService userService;
+
+    /**
+     * 강의 등록
+     * @param lecture 등록할 Lecture 객체
+     * @return true: 등록 성공, false: 등록 실패
+     */
+    @Transactional
+    public Boolean registerLecture(Lecture lecture){
+
+        if(existsLecture(lecture.getLectureCode())){
+            return false;
+        }
+
+        jpaLectureRepository.save(lecture);
+
+        return true;
+    }
+
+
+    /**
+     * 강의 코드를 이용하여 등록된 강의인지 확인
+     * @param lectureCode 강의 코드
+     * @return true: 존재함, false: 존재하지 않음
+     */
+    public Boolean existsLecture(Long lectureCode) {
+        return jpaLectureRepository.existsByLectureCode(lectureCode);
+    }
+
+
+    public Lecture transferToLecture(LectureRegisterForm form) {
+        Lecture lecture = new Lecture().builder()
+                .lectureCode(form.getLectureCode())
+                .professor((Professor)userService.getUser(form.getProfessorId()))
+                .lectureName(form.getLectureName())
+                .lectureGrade(form.getLectureGrade())
+                .lectureTimes(form.getLectureTimes())
+                .lectureRoom(form.getLectureRoom())
+                .lectureGrades(form.getLectureGrades())
+                .lectureKind(form.getLectureKind())
+                .lectureFull(form.getLectureFull())
+                .dayOrNight(form.getDayOrNight())
+                .build();
+        return lecture;
+    }
+
+
+
+
+
+}
