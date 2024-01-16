62ed414d41eb4dd576cdc126d5f835058f8e1d0d
JIUNG YANG <yangjiung286@gmail.com>
2024-01-16 18:10:34 +0900

    CHECKPASS-154 Feat : [ Server ] 강의 등록 구현

--- backend/src/main/java/FXPROJECT/CHECKPASS/web/service/lectures/LectureService.java
+++ backend/src/main/java/FXPROJECT/CHECKPASS/web/service/lectures/LectureService.java
@@ -1,14 +1,17 @@
 package FXPROJECT.CHECKPASS.web.service.lectures;
 
+import FXPROJECT.CHECKPASS.domain.entity.lectures.Lecture;
+import FXPROJECT.CHECKPASS.domain.entity.users.Professor;
 import FXPROJECT.CHECKPASS.domain.repository.lectures.JpaLectureRepository;
-import FXPROJECT.CHECKPASS.web.common.utils.ResultSetUtils;
+import FXPROJECT.CHECKPASS.web.common.utils.ResultFormUtils;
+import FXPROJECT.CHECKPASS.web.form.requestForm.lectures.register.LectureRegisterForm;
 import FXPROJECT.CHECKPASS.web.form.responseForm.resultForm.ResultForm;
+import FXPROJECT.CHECKPASS.web.service.users.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import static FXPROJECT.CHECKPASS.domain.common.constant.CommonMessage.*;
-import static FXPROJECT.CHECKPASS.domain.common.constant.State.*;
 import static FXPROJECT.CHECKPASS.domain.common.constant.ErrorCode.*;
 
 @Slf4j
@@ -17,6 +20,25 @@ import static FXPROJECT.CHECKPASS.domain.common.constant.ErrorCode.*;
 public class LectureService {
 
     private final JpaLectureRepository jpaLectureRepository;
+    private final UserService userService;
+
+    /**
+     * 강의 등록
+     * @param lecture 등록할 Lecture 객체
+     * @return true: 등록 성공, false: 등록 실패
+     */
+    @Transactional
+    public Boolean registerLecture(Lecture lecture){
+
+        if(existsLecture(lecture.getLectureCode())){
+            return false;
+        }
+
+        jpaLectureRepository.save(lecture);
+
+        return true;
+    }
+
 
     /**
      * 강의 삭제
@@ -26,12 +48,13 @@ public class LectureService {
     @Transactional
     public ResultForm deleteLecture(Long lectureCode){
         if(!existsLecture(lectureCode)){
-            return ResultSetUtils.getResultForm(FAIL, NON_EXISTING_LECTURE.getCode(), NON_EXISTING_LECTURE.getTitle(), FAIL_DELETE.getDescription(), null);
+            return ResultFormUtils.getFailResultForm(NON_EXISTING_LECTURE.getCode(), NON_EXISTING_LECTURE.getTitle(), FAIL_DELETE.getDescription(), null);
         }
         jpaLectureRepository.deleteLectureByLectureCode(lectureCode);
 
-        return ResultSetUtils.getResultForm(SUCCESS, OK.getCode(), OK.getTitle(), COMPLETE_DELETE.getDescription(), null);
+        return ResultFormUtils.getSuccessResultForm(COMPLETE_DELETE.getDescription());
     }
+
 
     /**
      * 강의 코드를 이용하여 등록된 강의인지 확인
@@ -41,4 +64,21 @@ public class LectureService {
     public Boolean existsLecture(Long lectureCode) {
         return jpaLectureRepository.existsByLectureCode(lectureCode);
     }
+
+    public Lecture transferToLecture(LectureRegisterForm form) {
+        Lecture lecture = new Lecture().builder()
+                .lectureCode(form.getLectureCode())
+                .professor((Professor)userService.getUser(form.getProfessorId()))
+                .lectureName(form.getLectureName())
+                .lectureGrade(form.getLectureGrade())
+                .lectureTimes(form.getLectureTimes())
+                .lectureRoom(form.getLectureRoom())
+                .lectureGrades(form.getLectureGrades())
+                .lectureKind(form.getLectureKind())
+                .lectureFull(form.getLectureFull())
+                .dayOrNight(form.getDayOrNight())
+                .build();
+        return lecture;
+    }
+
 }
