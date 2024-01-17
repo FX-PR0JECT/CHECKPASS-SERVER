c466bac6cf57d18086b60569a8a28d7bf1e254da
shinyeongwoon <duddns119@naver.com>
2024-01-17 18:10:53 +0900

    CHECKPASS-173 Feat : [ Server ] College & Department List 반환 구현

--- backend/src/main/java/FXPROJECT/CHECKPASS/domain/enums/CollegesEnum.java
+++ backend/src/main/java/FXPROJECT/CHECKPASS/domain/enums/CollegesEnum.java
@@ -2,24 +2,30 @@ package FXPROJECT.CHECKPASS.domain.enums;
 
 public enum CollegesEnum {
 
-    ConvergenceTechnology("융합기술대학"),
-    Engineering("공과대학"),
-    HumanitiesSocialSciences("인문사회대학"),
-    HealthLifeSciences("보건생명대학"),
-    Railway("철도대학"),
-    FutureConvergence("미래융합대학"),
-    FacultyOfLiberalArts("교양학부"),
-    Free("자유전공학부"),
-    CreativeConvergence("창의융합학부");
+    ConvergenceTechnology("융합기술대학",1l),
+    Engineering("공과대학",2l),
+    HumanitiesSocialSciences("인문사회대학",3l),
+    HealthLifeSciences("보건생명대학",4l),
+    Railway("철도대학",5l),
+    FutureConvergence("미래융합대학",6l),
+    FacultyOfLiberalArts("교양학부",7l),
+    Free("자유전공학부",8l),
+    CreativeConvergence("창의융합학부",9l);
 
 
     private String college;
+    private Long collegeCode;
 
-    CollegesEnum(String college) {
+    CollegesEnum(String college, Long collegeCode) {
         this.college = college;
+        this.collegeCode = collegeCode;
     }
 
     public String getCollege() {
         return college;
+    }
+
+    public Long getCollegeCode() {
+        return collegeCode;
     }
 }
--- backend/src/main/java/FXPROJECT/CHECKPASS/domain/repository/college/JpaDepartmentRepository.java
+++ backend/src/main/java/FXPROJECT/CHECKPASS/domain/repository/college/JpaDepartmentRepository.java
@@ -2,9 +2,16 @@ package FXPROJECT.CHECKPASS.domain.repository.college;
 
 import FXPROJECT.CHECKPASS.domain.entity.college.Departments;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface JpaDepartmentRepository extends JpaRepository<Departments,Long> {
+
     Optional<Departments> findByDepartment(String department);
+
+    @Query(value = "select d from Departments d where d.colleges.collegeId = :collegeId")
+    Optional<Departments> findDepartmentsByColleges(@Param("collegeId") Long collegeId);
+
 }
