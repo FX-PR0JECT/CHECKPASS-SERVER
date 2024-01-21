b86a50ac9ebff3596754e667a7c50c2fce9dd774
JIUNG YANG <yangjiung286@gmail.com>
2024-01-21 20:32:18 +0900

    CHECKPASS-155 Feat : [ Server ] 강의 수정 구현

--- backend/src/main/java/FXPROJECT/CHECKPASS/web/controller/LectureController.java
+++ backend/src/main/java/FXPROJECT/CHECKPASS/web/controller/LectureController.java
@@ -4,8 +4,11 @@ import FXPROJECT.CHECKPASS.domain.common.exception.ExistingLecture;
 import FXPROJECT.CHECKPASS.domain.common.exception.NoPermission;
 import FXPROJECT.CHECKPASS.domain.common.exception.NonExistingLecture;
 import FXPROJECT.CHECKPASS.domain.entity.lectures.Lecture;
+import FXPROJECT.CHECKPASS.domain.entity.users.Users;
+import FXPROJECT.CHECKPASS.web.common.annotation.LoginUser;
 import FXPROJECT.CHECKPASS.web.common.utils.ResultFormUtils;
 import FXPROJECT.CHECKPASS.web.form.requestForm.lectures.register.LectureRegisterForm;
+import FXPROJECT.CHECKPASS.web.form.requestForm.lectures.update.LectureUpdateForm;
 import FXPROJECT.CHECKPASS.web.form.responseForm.resultForm.ResultForm;
 import FXPROJECT.CHECKPASS.web.service.lectures.LectureService;
 import lombok.RequiredArgsConstructor;
@@ -26,7 +29,6 @@ public class LectureController {
 
     @PostMapping("/registerLecture")
     public ResultForm registerLecture(@RequestBody @Validated LectureRegisterForm form, BindingResult bindingResult){
-
         Lecture lecture = lectureService.transferToLecture(form);
 
         if(!lectureService.registerLecture(lecture)){
@@ -35,6 +37,28 @@ public class LectureController {
 
         return ResultFormUtils.getSuccessResultForm(COMPLETE_REGISTER.getDescription());
 
+    }
+
+    @PatchMapping("/{lectureCode}")
+    public ResultForm editLectureInformation(@PathVariable("lectureCode") Long lectureCode, @RequestBody LectureUpdateForm form, @LoginUser Users loggedInUser) {
+
+        if (!lectureService.existsLecture(lectureCode)){
+            throw new NonExistingLecture();
+        }
+
+        Lecture target = lectureService.getLecture(lectureCode);
+
+        if (!loggedInUser.getDepartments().getDepartment().equals(target.getDepartments().getDepartment())) {
+            throw new NoPermission();
+        }
+
+        if (!loggedInUser.getUserId().equals(target.getProfessor().getUserId())){
+            throw new NoPermission();
+        }
+
+        lectureService.editLectureInformation(target, form);
+
+        return ResultFormUtils.getSuccessResultForm(COMPLETE_UPDATE.getDescription());
     }
 
     @DeleteMapping("/{lectureCode}")
---
+++ backend/src/main/java/FXPROJECT/CHECKPASS/web/form/requestForm/lectures/update/LectureUpdateForm.java
@@ -0,0 +1,29 @@
+package FXPROJECT.CHECKPASS.web.form.requestForm.lectures.update;
+
+import FXPROJECT.CHECKPASS.domain.enums.LectureKind;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class LectureUpdateForm {
+
+    private Long professorId;
+
+    private String lectureName;
+
+    private String lectureTimes;
+
+    private String lectureRoom;
+
+    private String lectureGrade;
+
+    private LectureKind lectureKind;
+
+    private String lectureGrades;
+
+    private Integer lectureFull;
+
+    private String dayOrNight;
+}
