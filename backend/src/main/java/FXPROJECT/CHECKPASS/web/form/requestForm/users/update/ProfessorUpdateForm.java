84ae137772b39be02631f74a845f942ac945be6b
shinyeongwoon <duddns119@naver.com>
2024-02-08 10:02:14 +0900

    CHECKPASS-233 Feat : [ Server ] Professor UpdateForm 추가

---
+++ backend/src/main/java/FXPROJECT/CHECKPASS/web/form/requestForm/users/update/ProfessorUpdateForm.java
@@ -0,0 +1,12 @@
+package FXPROJECT.CHECKPASS.web.form.requestForm.users.update;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProfessorUpdateForm extends UpdateForm {
+
+    private String hireDate;
+
+}
